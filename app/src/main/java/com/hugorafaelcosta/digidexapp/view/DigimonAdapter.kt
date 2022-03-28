d9028ff3ccdd2fc21edafcc631d077e35956c12c
Hugo Costa <costarafaelhugo@gmail.com>
2022-03-28 16:06:55 -0300

    Finalizei a base chamando o recyclerView já populado

--- app/src/main/java/com/hugorafaelcosta/digidexapp/view/DigimonAdapter.kt
+++ app/src/main/java/com/hugorafaelcosta/digidexapp/view/DigimonAdapter.kt
@@ -1,18 +1,21 @@
 package com.hugorafaelcosta.digidexapp.view
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.hugorafaelcosta.digidexapp.R
 import com.hugorafaelcosta.digidexapp.domain.Digimon
 
 class DigimonAdapter(
-    val items: List<Digimon>
+    private val items: List<Digimon>
 ) : RecyclerView.Adapter<DigimonAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.digimon_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.digimon_item, parent, false)
 
         return ViewHolder(view)
     }
@@ -22,15 +25,23 @@ class DigimonAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
 
+        holder.bindView(item)
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindView() {
+        @SuppressLint("SetTextI18n")
+        fun bindView(item: Digimon) = with(itemView) {
+            val digimonImageView = findViewById<ImageView>(R.id.digimonImageView)
+            val digimonNameTextView = findViewById<TextView>(R.id.digimonNameTextView)
+            val digimonLevelTextView = findViewById<TextView>(R.id.digimonLevelTextView)
 
+            //TODO: Load image with Glide
+
+            digimonNameTextView.text = "Nome ${item.name}"
+            digimonLevelTextView.text = "Level ${item.level}"
         }
     }
-
-
 }
--- app/src/main/java/com/hugorafaelcosta/digidexapp/view/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/digidexapp/view/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.hugorafaelcosta.digidexapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hugorafaelcosta.digidexapp.R
+import com.hugorafaelcosta.digidexapp.domain.Digimon
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewDigimons)
+
+        val agumon = Digimon(
+            "https://digimon.shadowsmith.com/img/agumon.jpg",
+            "Rookie",
+            "Agumon"
+        )
+        val digimons = listOf(
+            agumon, agumon, agumon, agumon, agumon
+        )
+
+        val layoutManager = LinearLayoutManager(this)
+
+        recyclerView.layoutManager = layoutManager
+
+        recyclerView.adapter = DigimonAdapter(digimons)
     }
 }
